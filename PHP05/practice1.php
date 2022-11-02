cf6b830fec8a0b5e60ad1bc9fb2f3026992f67e8
y-choken <ch.apl.ln_yr@icloud.com>
2022-11-02 12:40:15 +0000

    PHP05 課題

---
+++ PHP05/practice1.php
@@ -0,0 +1,8 @@
+<?php
+$myName = "屋良朝健";
+$name = "屋良朝健";
+if ($name == $myNme){
+    echo "私は" . $myName . "です";
+} else {
+    echo "私は" . $myName . "ではありません";
+}
---
+++ PHP05/practice3.php
@@ -0,0 +1,6 @@
+<?php
+$fruits = array("pear","apple","orange","melon","watermelon");
+foreach($fruits as $fruit){
+    echo $fruit;
+    echo "\n";
+}
